d47b8b3048388312792cd40ddb61d3775aec350a
rajahwu <vmradford@gmail.com>
2023-08-17 11:52:58 -0400

    add read to user readable rating

--- app/_components/ratings.tsx
+++ app/_components/ratings.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { rateReadable } from "../_utils/rate-readable";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const Star = ({ handleClick }: { handleClick: () => void }) => {
   return (
@@ -13,6 +15,29 @@ const Star = ({ handleClick }: { handleClick: () => void }) => {
 
 export default function Rating({ readable }: { readable: ReadableWithReader }) {
   const router = useRouter();
+  const supabase = createClientComponentClient();
+  const [userRating, setUserRating] = useState(null);
+
+  useEffect(() => {
+    const getUserRating = async () => {
+      const { data } = await supabase
+        .from("ratings")
+        .select()
+        .eq("reader_id", readable.reader_id)
+        .eq("readable_id", readable.id)
+        .limit(1);
+
+      // console.log(JSON.stringify(rating, null, 2));
+      if (data) {
+        const stars = Array.isArray(data) ? data[0] : data;
+        // console.log(stars?.stars);
+        setUserRating(stars?.stars);
+      }
+    };
+    getUserRating();
+  }, []);
+
+  // console.log(userRating)
 
   const handleClick = (numOfStars: number) => {
     rateReadable(readable, numOfStars);
@@ -20,10 +45,8 @@ export default function Rating({ readable }: { readable: ReadableWithReader }) {
   };
   return (
     <div className="bg-amber-600 flex-1">
-      <p>Reader rating: </p>
-      <p>
-        Ratings: {readable.ratings.length} {}
-      </p>
+      <p>Reader rating: {userRating}</p>
+      <p>Ratings: {readable.ratings.length}</p>
 
       <div className="flex p-3">
         <Star
